e98b9cabb58918edde9e9eafdd187c337342f7b7
Azizkhan22 <azizkhan22kakar@gmail.com>
2025-10-05 00:45:00 +0500

    dev: continue

--- server/src/services/neo4jService.js
+++ server/src/services/neo4jService.js
@@ -93,7 +93,7 @@ async function getTopEntitiesWithRelations(limit = 40) {
           });
         }
       }
-  
+      
       return {
         entities: Array.from(entitiesMap.values()),
         relations,
